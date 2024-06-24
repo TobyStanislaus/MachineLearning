186895f15fdc9dc433339bd17e68e0f1b5a53a20
Tobyisepik <116378333+Tobyisepik@users.noreply.github.com>
2024-06-24 22:26:04 +0100

    got bugs now:(

--- firstProject.py
+++ firstProject.py
@@ -11,7 +11,6 @@ from IPython.display import clear_output
 #from six.moves import urllib
 
 import tensorflow as tf
-from tensorflow.keras import layers
 
 dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv')
 dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
@@ -38,4 +37,17 @@ for feature_name in NUMERIC_COLUMNS:
   feature_columns.append(tf.feature_column.numeric_column(feature_name,dtype=tf.float32))
 
 
-print(feature_columns)
+def make_input_fn(data_df,label_df, num_epochs=10, shuffle=True, batch_size=32):
+  def input_function():
+    ds = tf.data.Dataset.from_tensor_slices((dict(data_df),label_df))
+
+    if shuffle:
+      ds=ds.shuffle(1000)
+    ds = ds.batch(batch_size).repeat(num_epochs)
+    return ds
+  return input_function
+
+train_input_fn = make_input_fn(dftrain, y_train)
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
