bb65f373b4b39380eef0d761ccd5b617722a7854
Tobyisepik <116378333+Tobyisepik@users.noreply.github.com>
2024-06-28 22:00:30 +0100

    I THINK IMMA GO DO IT FR - I STILL GOT IT

--- firstProject.py
+++ firstProject.py
@@ -1,53 +1,49 @@
-from __future__ import absolute_import,division,print_function,unicode_literals
-
-import os
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-
-import numpy as np
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-
-from IPython.display import clear_output
-#from six.moves import urllib
+import seaborn as sns
 
 import tensorflow as tf
+from tensorflow import keras
 
-dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv')
-dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
-#print(dftrain.head())
-
-y_train = dftrain.pop('survived')
-y_eval = dfeval.pop('survived')
+data=pd.read_csv('train.csv')
 
 
-#print(dftrain.head())
-
-CATEGORICAL_COLUMNS = ['sex','n_siblings_spouses','parch','class','deck',
-                       'embark_town','alone']
-
-NUMERIC_COLUMNS = ['age','fare']
-
-feature_columns = []
-for feature_name in CATEGORICAL_COLUMNS:
-  vocab = dftrain[feature_name].unique()
-
-  feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name,vocab))
-
-for feature_name in NUMERIC_COLUMNS:
-  feature_columns.append(tf.feature_column.numeric_column(feature_name,dtype=tf.float32))
+data.Age = data.Age.astype(float)
+data.Fare = data.Fare.astype(float)
+data.fillna(0)
 
 
-def make_input_fn(data_df,label_df, num_epochs=10, shuffle=True, batch_size=32):
-  def input_function():
-    ds = tf.data.Dataset.from_tensor_slices((dict(data_df),label_df))
+data['Fare'] = data['Fare'].fillna(data['Fare'].median())
+data['Age'] = data['Age'].fillna(data['Age'].median())
 
-    if shuffle:
-      ds=ds.shuffle(1000)
-    ds = ds.batch(batch_size).repeat(num_epochs)
-    return ds
-  return input_function
 
-train_input_fn = make_input_fn(dftrain, y_train)
-eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
 
-linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+'''
+plt.figure(figsize=(10,10))
+ax = plt.subplot()
+ax.scatter(data[data['Survived'] == 1]['Age'],data[data['Survived'] == 1]['Fare'], c='green', s=data[data['Survived'] == 1]['Fare'] )
+
+ax.scatter(data[data['Survived'] == 0]['Age'],data[data['Survived'] == 0]['Fare'], c='red', s=data[data['Survived'] == 0]['Fare'] )
+
+plt.show()
+'''
+
+sex1={'male':1, 'female':2}
+data.Sex=data.Sex.map(sex1)
+
+data.Age = data.Age.astype(int)
+data.Fare = data.Fare.astype(int)
+
+y=data["Survived"]
+features = ['Pclass','Sex','Age','Parch','Fare']
+
+X=data[features]
+
+
+X = ((X-X.min()) / (X.max() - X.min()))
+
+print(X)
+
+
+
